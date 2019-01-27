5a19480ebee300eda1eb01929b66888e938b55c1
saikatghosh1998 <saikatghosh1998@gmail.com>
2019-01-27 19:31:05 +0530

    Error Handling

--- dictionary.py
+++ dictionary.py
@@ -1,28 +1,43 @@
 import sys
 import requests
-import re
 from bs4 import BeautifulSoup as bs
 
 url = "https://www.merriam-webster.com/dictionary/"
 
-word = sys.argv[1]
-url += word
+try:
+    word = sys.argv[1]
+    url += word
+except:
+    print("Please Specify a Word !!")
+    exit(-1)
+    
+try:
+    r = requests.get(url)
+    soup = bs(r.content, 'lxml')
+except:
+    print("Please check you internet connection !!")
+    exit(-1)
 
-r = requests.get(url)
-soup = bs(r.content, 'lxml')
-
-pos = soup.findAll("span", {"class": "fl"})[0].text
-pronounce=soup.findAll("span",{"class":"pr"})[0].text
-answer_list = soup.findAll("div", {"class": "vg"})[0]
-meanings = answer_list.findAll("span",{"class":"dtText"})
-#pos=meanings.findAll("a",{"class":"anchor"}).text
-print("P.O.S" + ": "+pos)
-print("pronounciation : "+pronounce)
 
 
+try:
+    pronounce=soup.findAll("span",{"class":"pr"})[0].text
+except:
+    pronounce="NA"
+    
+try:
+    pos = soup.findAll("span", {"class": "fl"})[0].textpos = soup.findAll("span", {"class": "fl"})[0].text
+    answer_list = soup.findAll("div", {"class": "vg"})[0]
+    meanings = answer_list.findAll("span",{"class":"dtText"})
+except:
+    print("Word Not Found !!")
+    exit(-1)
+
+
+print()
+print("P.O.S" + ": "+pos+"     |     "+"pronunciation: "+pronounce)
 
 #d=[]
 for (i, meaning) in enumerate(meanings):
     print()
-    
-    print(str(i + 1) + ".", meaning.get_text(strip=True)) 
+    print(str(i + 1) + ".", meaning.text) 
